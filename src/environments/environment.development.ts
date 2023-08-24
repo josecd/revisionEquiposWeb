72c4836b90160ec7d80e09460306ce0060b8b8e0
Jose Alejandro <44279311+josecd@users.noreply.github.com>
2023-08-23 21:50:57 -0500

    app

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    API_URL: 'http://localhost:3000'
-    // API_URL: 'https://revisionequiposapi-production.up.railway.app'
+    // API_URL: 'http://localhost:3000'
+    API_URL: 'https://revisionequiposapi-production.up.railway.app'
   
 };
